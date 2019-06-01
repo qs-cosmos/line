608f68e4bcdd271f3a374e2c7c7c154ead4f1f40
qs-cosmos <qs.cosmos.pine@gmail.com>
2019-06-01 10:58:23 +0800

    Modify the order for the c++ include and add the include path of qt

--- env/vim/ycm_extra_conf.py
+++ env/vim/ycm_extra_conf.py
@@ -59,8 +59,11 @@ flags = [
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x', 'c++',
 '-I', '.',
-'-isystem', '/usr/include/c++/7.3.0',
-'-isystem', '/usr/lib/gcc/x86_64-linux-gnu/7.3.0/include',
+# For the QT project
+'-I', '/home/qingsong/program/qt/5.12.0/5.12.0/gcc_64/include',
+'-isystem', '/usr/include/c++/7',
+'-isystem', '/usr/lib/gcc/x86_64-linux-gnu/7/include',
+'-isystem', '/usr/include/x86_64-linux-gnu/',
 '-isystem', '/usr/local/include',
 '-isystem', '/usr/include',
 ]
